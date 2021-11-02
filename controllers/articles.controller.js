1409bf0f2d05abaa711e612dc3c17f29af78199d
emilyjevans94 <emilyjevans94@hotmail.com>
2021-11-02 12:40:16 +0000

    small change

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -3,7 +3,7 @@ const { selectArticle } = require("../models/articles.models.js")
 exports.getArticle = (req, res, next) => {
   console.log("in the controller");
   const { article_id } = req.params;
-  selectArticle(article_id)
+  return selectArticle(article_id)
   .then((data) => res.status(200).send({ article: data }))
   .catch(next);
 };
