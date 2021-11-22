40dbb84fe77a96f5318fa28e6cb07c777f75e7bf
emilyjevans94 <emilyjevans94@hotmail.com>
2021-11-22 14:43:38 +0000

    all cross origin resource sharing

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const { PSQLerror, customError } = require("./controllers/errors.controller");
 const app = express();
-const { apiRouter } = require("./routes/api.router")
+const { apiRouter } = require("./routes/api.router");
+const cors = require("cors");
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use('/api', apiRouter)
+app.use("/api", apiRouter);
 
-app.use(PSQLerror)
+app.use(PSQLerror);
 
-app.use(customError)
+app.use(customError);
 
 app.all("/*", (req, res) => {
-	res.status(404).send({ msg: "Not found" });
+  res.status(404).send({ msg: "Not found" });
 });
 
 module.exports = app;
